ea58449cb1a28d5ad1d674dc94d11779b6fc150f
MOHAMMAD HASSNAIN <hassnigithub@gmail.com>
2024-03-05 17:42:19 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
-
-import  { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import image from "./assets/OBJECTS.png";
 import "./App.css";
 import { fetchWinner } from "./api/winner-api";
-const App = () => {
- 
-  const tMax = 5000; // animation time, ms
-  const height = 210;
-  const reels = [
-    [3, 3, 3], // Reel 1
-    [4, 4, 4], // Reel 2
-    [5, 5, 5], // Reel 3
-    [6, 6, 6], // Reel 4
-    [7, 7, 7], // Reel 5
-    [8, 8, 8], // Reel 6
-    [9, 9, 9], // Reel 7
-    [0, 0, 0], // Reel 8
-    [2, 2, 2], // Reel 9
-    [1, 1, 1], // Reel 10
-  ];
 
-  const [isSpinning, setIsSpinning] = useState(false);
+const App = () => {
   const [winner, setWinner] = useState(null);
+  const tMax = 5000;
+  const height = 210;
+  let reels;
+  if (winner && winner?.data && winner?.data?.code) {
+    reels = winner?.data?.code
+      ?.split("")
+      ?.map((digit) => [parseInt(digit), parseInt(digit), parseInt(digit)]);
+  } else {
+    reels = [
+      [3, 3, 3], 
+      [4, 4, 4],
+      [5, 5, 5], 
+      [6, 6, 6], 
+      [7, 7, 7], 
+      [8, 8, 8],
+      [9, 9, 9], 
+      [0, 0, 0], 
+      [2, 2, 2], 
+      [1, 1, 1], 
+    ];
+  }
+  const [isSpinning, setIsSpinning] = useState(false);
   const [msg, setMsg] = useState("");
   const reelsRefs = useRef([]);
   const speeds = useRef([]);
@@ -30,40 +35,51 @@ const App = () => {
 
   const action = () => {
     if (isSpinning) return;
-
     setIsSpinning(true);
     setMsg("Spinning...");
     start.current = performance.now();
-
-    for (let i = 0; i < 10; ++i) {
+    const codeLength =
+      winner && winner.data && winner.data.code ? winner.data.code.length : 10;
+    for (let i = 0; i < codeLength; ++i) {
       speeds.current[i] = Math.random() + 0.5;
       r.current[i] = (((Math.random() * 3) | 0) * height) / 3;
     }
-
     animate(performance.now());
   };
 
   const animate = (now) => {
     if (!start.current) start.current = now;
     const t = now - start.current || 0;
+    const codeLength =
+      winner && winner.data && winner.data.code ? winner.data.code.length : 10;
 
-    for (let i = 0; i < 10; ++i) {
+    for (let i = 0; i < codeLength; ++i) {
       reelsRefs.current[i].scrollTop =
-        ((speeds.current[i] / tMax / 2) * (tMax - t) * (tMax - t) + r.current[i]) % height;
+        ((speeds.current[i] / tMax / 2) * (tMax - t) * (tMax - t) +
+          r.current[i]) %
+        height;
     }
-
     if (t < tMax) {
       requestAnimationFrame(animate);
     } else {
-      setIsSpinning(false); // Disable spinning when animation ends
-      setMsg("DD");
+      setIsSpinning(false);
+      setMsg(winner?.data?.full_name || winner?.message);
     }
   };
   useEffect(() => {
-    const response =fetchWinner()
-    setWinner(response.data);
-  },[])
-  console.log(winner,"winner");
+    const fetchWinnerData = async () => {
+      try {
+        const response = await fetchWinner();
+        // console.log(response, "resp");
+        setWinner(response?.data);
+      } catch (error) {
+        setWinner(error?.response?.data);
+      }
+    };
+
+    fetchWinnerData();
+  }, []);
+  // console.log(winner,"winner");
   return (
     <div id="sm">
       <h1>Slot Machine</h1>
@@ -72,8 +88,12 @@ const App = () => {
         <p className="msg">{msg}</p>
       </div>
       <div className="group">
-        {reels.map((reel, index) => (
-          <div key={index} className="reel" ref={(el) => (reelsRefs.current[index] = el)}>
+        {reels?.map((reel, index) => (
+          <div
+            key={index}
+            className="reel"
+            ref={(el) => (reelsRefs.current[index] = el)}
+          >
             {reel.map((number, rowIndex) => (
               <div key={rowIndex}>
                 {reel.map((num, columnIndex) => (
--- src/api/winner-api.js
+++ src/api/winner-api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 export const URL_API = import.meta.env.VITE_HOST_API;
 
 export const fetchWinner = async () => {
-    return await axios.post(`${URL_API}/api/get_winner`);
+    return await axios.post(`https://new.fustekaice.com/api/get_winner`);
   };
   
